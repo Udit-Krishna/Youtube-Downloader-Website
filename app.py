f917704b57ea42884deb5176d2f248fcd982a968
Udit Krishna <uditkrishna2003@gmail.com>
2021-04-15 16:05:40 +0530

    hehe

--- app.py
+++ app.py
@@ -1,27 +1,55 @@
 from flask import Flask
 from flask import request 
 from flask import render_template
-from pytube import YouTube
+import youtube_dl
+import os
 
 app = Flask(__name__,static_url_path='/static')
 
-vid = None
+@app.route('/final_vid')
+def final_vid():
+    files = os.listdir('static') 
+    for a in files:
+        if a.split('.')[0] == 'download':
+            fo = a
+    return render_template('final_video.html', filename = fo)
 
-@app.route('/final')
-def winners(): 
-    return render_template('final.html')
+@app.route('/final_aud')
+def final_aud():
+    files = os.listdir('static') 
+    for a in files:
+        if a.split('.')[0] == 'download':
+            fo = a
+    return render_template('final_video.html', filename = fo)
 
 @app.route('/',methods=['POST','GET'])
 def send():
     if request.method== 'POST':
         link=request.form['link']
-        print(link)
-        global vid
+        fmt=request.form['options']
+        if not fmt:
+            fmt = 'video'
+        print(link,fmt)
         try:
-            video = YouTube(link)
-            print(video.streams)
-            video.streams.get_highest_resolution().download(output_path="static", filename="download")
-            return render_template('final.html')
+            os.remove('static/download.mp3')
+        except:
+            pass
+        try:
+            os.remove('static/download.mkv')
+        except:
+            pass
+        try:
+            aydl_opts = {'format': 'bestaudio/best',
+                         'outtmpl': 'static/download.mp3',
+                        'extractaudio': True,
+                        'audioformat': "mp3",}
+            if fmt == 'audio':
+                with youtube_dl.YoutubeDL(aydl_opts) as ydl:
+                    ydl.download([f'{link}'])
+                    return render_template('final_audio.html')
+            if fmt == 'video':
+                os.system(f'youtube-dl -f "bestvideo[filesize<500M][height<=?720]+bestaudio/best" -o "/static/download" --no-continue {link}')
+                return render_template('final_video.html')
         except:
             return render_template('errorpage.html')
     return render_template("main.html")
